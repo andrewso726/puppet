c3eccdcfca7856b40dd1647726e74cde95817d45
andrewso726 <andrewso726@gmail.com>
2016-07-20 20:11:27 -0400

    adding vcsrepo

--- modules/my_shit/manifests/init.pp
+++ modules/my_shit/manifests/init.pp
@@ -11,4 +11,10 @@ class my_shit {
   package { 'puppet':
     ensure => '3.4.3-1ubuntu1.1',
   }
+
+  vcsrepo { '/root':
+    ensure   => present,
+    provider => git,
+    source   => 'git@github.com:andrewso726/homedir.git',
+  }
 }
